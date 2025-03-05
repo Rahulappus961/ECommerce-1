586ff6394615ecc7bc4aa4ef70ec426d4a79d834
Rahulappus961 <rahulappus961@gmail.com>
2025-03-05 16:36:03 +0530

    update Project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.render(
+  <Router basename='/'>
     <App />
-  </StrictMode>,
+  </Router>,
+  document.getElementById("root")
 )
