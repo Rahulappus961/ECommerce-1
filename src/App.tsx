dae5661d60a192f3e8559da9b6c2e354602c2787
Rahul.R <rahulappus961@gmail.com>
2025-03-05 12:30:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './index.css'
 import Create from './Components/Create'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
